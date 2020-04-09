8891e2b03ef861a90ebec70a9697168dc4d57c5d
liuwei <2455621760@qq.com>
2020-04-09 19:01:27 +0800

    first commit

---
+++ src/test/java/com/commity/backmethod/BackmethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commity.backmethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackmethodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
